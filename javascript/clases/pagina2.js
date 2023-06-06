c80e3a75e9d4b504a71dbc7dd56a5d03a899c7dd
ecram <marcelopalma@ic.unicamp.br>
2023-06-06 16:12:48 -0400

    2023-06-06 FInal JavaScript

---
+++ javascript/clases/pagina2.js
@@ -0,0 +1,13 @@
+function retornarFecha() 
+{
+    var fecha = new Date();
+    var cadena = fecha.getDate() + '/' + (fecha.getMonth() + 1) + '/' + fecha.getFullYear();
+    return cadena;
+}
+
+function retornarHora() 
+{
+    var fecha = new Date();
+    var cadena = fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
+    return cadena;
+}
